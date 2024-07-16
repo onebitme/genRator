28bdb1ec3b1d5fd2768e01fcb4ea08bbc5a39ae9
Ersun Sözen <ersunsozen@Ersun-MacBook-Pro.local>
2024-07-16 13:43:53 +0300

    Dotenv + .env setup

--- genrator_big.py
+++ genrator_big.py
@@ -4,3 +4,7 @@ import asyncio
 import pandas as pd
 from openai import AsyncOpenAI
 from math import isnan
+
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+load_dotenv()
